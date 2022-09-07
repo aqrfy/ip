f234dd662897d74e2b1e292f1edcd6547023f000
aqrfy <113006423+aqrfy@users.noreply.github.com>
2022-09-07 09:09:46 +0300

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,4 @@
+import socket as s 
+host = input ("\033[1;37m الاسود any nebsite : ")
+print (f'IP of (host) is {s.gethostbyname(host)}')                     
+                                                                       
