db47012a862d873caf94764ef3fef495a8ef0396
aqrfy <113006423+aqrfy@users.noreply.github.com>
2022-09-07 09:26:41 +0300

    Delete ip.py

--- ip.py
+++
@@ -1,4 +0,0 @@
-import socket as s 
-host = input ("\033[1;37m الاسود any nebsite : ")
-print (f'IP of (host) is {s.gethostbyname(host)}')                     
-                                                                       
